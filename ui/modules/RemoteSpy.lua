0834681cd266acfe4901694ad20ad158f09b9ccb
Delworld <r7162419@gmail.com>
2026-07-12 07:03:05 +1100

    - fixed small bugs

--- ui/modules/RemoteSpy.lua
+++ ui/modules/RemoteSpy.lua
@@ -61,6 +61,7 @@ local icons = {
     ignore = "rbxassetid://4842578510",
     unignore = "rbxassetid://4842578818",
     RemoteEvent = "rbxassetid://4229806545",
+    UnreliableRemoteEvent = "rbxassetid://4229806545",
     RemoteFunction = "rbxassetid://4229810474",
     BindableEvent = "rbxassetid://4229809371",
     BindableFunction = "rbxassetid://4229807624"
@@ -434,11 +435,11 @@ function Log.adjust(log)
     local logIcon = logInstance.Icon
 
     local callWidth = TextService:GetTextSize(logInstance.Calls.Text, 18, "SourceSans", constants.textWidth).X + 10
-    local iconPosition = callWidth - (((remoteClassName == "RemoteEvent" or remoteClassName == "BindableEvent") and 4) or 0)
+    local iconPosition = callWidth - (((remoteClassName == "RemoteEvent" or remoteClassName == "UnreliableRemoteEvent" or remoteClassName == "BindableEvent") and 4) or 0)
     local labelWidth = iconPosition + 21
 
     logInstance.Calls.Size = UDim2.new(0, callWidth, 1, 0)
-    logIcon.Position = UDim2.new(0, iconPosition, 0.5, (remoteClassName == "RemoteEvent" and -9) or -7)
+    logIcon.Position = UDim2.new(0, iconPosition, 0.5, ((remoteClassName == "RemoteEvent" or remoteClassName == "UnreliableRemoteEvent") and -9) or -7)
     logInstance.Label.Position = UDim2.new(0, labelWidth, 0, 0)
     logInstance.Label.Size = UDim2.new(1, -labelWidth, 1, 0)
 end
@@ -837,7 +838,7 @@ scriptContext:SetCallback(function()
     local remotePath = getInstancePath(selectedRemote)
     local method
 
-    if remoteClassName == "RemoteEvent" then
+    if remoteClassName == "RemoteEvent" or remoteClassName == "UnreliableRemoteEvent" then
         method = "FireServer"
     elseif remoteClassName == "RemoteFunction" then
         method = "InvokeServer"
@@ -911,7 +912,7 @@ repeatCallContext:SetCallback(function()
     local remoteClassName = remoteInstance.ClassName
     local method 
 
-    if remoteClassName == "RemoteEvent" then
+    if remoteClassName == "RemoteEvent" or remoteClassName == "UnreliableRemoteEvent" then
         method = "FireServer"
     elseif remoteClassName == "RemoteFunction" then
         method = "InvokeServer"
